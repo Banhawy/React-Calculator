8c3ea366285ad1e7317f2ebdad4c43df9865dcf6
Adham El Banhawy <the.benhawy@gmail.com>
2020-09-08 16:55:45 +0200

    Initial commit

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function () {
+  return (
+    <div>
+      <button id="equals">=</button>
+    </div>
+  );
+}
---
+++ src/components/calculator.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Buttons from "./Buttons";
+
+export default function () {
+  return (
+    <div className="container">
+      Calculator container
+      <div id="buttons">
+        <Buttons />
+      </div>
+    </div>
+  );
+}
